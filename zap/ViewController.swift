5bc0dc3a6b02c1ffa11d7688324db8e582fdb7d7
samedkarakus <samedkarakus.ceng@gmail.com>
2024-09-30 00:02:52 +0300

    UI dev

--- zap/ViewController.swift
+++ zap/ViewController.swift
@@ -8,12 +8,34 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var streakView: UIImageView!
+    @IBOutlet weak var learningNowView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        makeCircular(view: learningNowView, borderColor: UIColor.white, borderWidth: 0.5)
+        addBlurredBackground(learningNowView)
+        
+        makeCircular(view: streakView, borderColor: UIColor.white, borderWidth: 0.5)
+        addBlurredBackground(streakView)
     }
-
-
+    
+    func makeCircular(view: UIView, borderColor: UIColor, borderWidth: CGFloat) {
+        view.layer.masksToBounds = true
+        view.layer.cornerRadius = view.frame.height / 2
+        view.layer.borderColor = UIColor.white.cgColor
+        view.layer.borderWidth = 0.5
+    }
+    
+    func addBlurredBackground(_ view: UIView) {
+        let blurEffectView = UIVisualEffectView(effect: UIBlurEffect(style: .light))
+        blurEffectView.frame = view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        view.insertSubview(blurEffectView, at: 0)
+        blurEffectView.alpha = 0.9
+    }
 }
 
