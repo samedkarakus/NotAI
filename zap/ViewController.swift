4807946a62b746dd7e5bfcf2ccb444c1da6374f2
samedkarakus <samedkarakus.ceng@gmail.com>
2024-09-29 19:58:59 +0300

    Initial Commit

---
+++ zap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zap
+//
+//  Created by Samed Karakuş on 29.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
