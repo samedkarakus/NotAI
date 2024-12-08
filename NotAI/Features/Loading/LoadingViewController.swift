7ccaf6a449a242d746b9849c1128b9b39b7d1b1c
samedkarakus <samedkarakus.ceng@gmail.com>
2024-12-08 22:54:31 +0300

    loading scene animation added.

--- NotAI/Features/Loading/LoadingViewController.swift
+++ NotAI/Features/Loading/LoadingViewController.swift
@@ -11,19 +11,24 @@ class LoadingViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadDataAndTransition()
+    }
 
-        // Do any additional setup after loading the view.
+    private func loadDataAndTransition() {
+        DispatchQueue.global().async {
+            sleep(3)
+            
+            DispatchQueue.main.async { [weak self] in
+                self?.transitionToMainScreen()
+            }
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func transitionToMainScreen() {
+        if let mainVC = storyboard?.instantiateViewController(withIdentifier: "HomeViewController") {
+            mainVC.modalPresentationStyle = .fullScreen
+            present(mainVC, animated: true, completion: nil)
+        }
     }
-    */
-
 }
+
