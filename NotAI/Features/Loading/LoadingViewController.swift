42d77d4f7254ecc2fd2f240c0332a616d6a94ae1
samedkarakus <samedkarakus.ceng@gmail.com>
2024-12-08 19:12:37 +0300

    loading secton added.

---
+++ NotAI/Features/Loading/LoadingViewController.swift
@@ -0,0 +1,29 @@
+//
+//  LoadingViewController.swift
+//  NotAI
+//
+//  Created by Samed Karakuş on 8.12.2024.
+//
+
+import UIKit
+
+class LoadingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
